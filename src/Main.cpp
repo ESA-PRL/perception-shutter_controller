ae56c3a9d79d06c5466f5712d57063f6bb5b86d6
Levin Gerdes <gerdeslevin@gmail.com>
2017-03-06 10:02:40 +0100

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <shutter_controller/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    shutter_controller::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
